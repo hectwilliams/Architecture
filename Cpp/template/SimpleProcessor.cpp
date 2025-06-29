eb0648e1b1ac127af14bba523183abc2cfa42e5d
Hector <hectwilliams@gmail.com>
2025-06-29 17:53:18 -0400

    updated slide deck and light code refactoring

--- Cpp/template/SimpleProcessor.cpp
+++ Cpp/template/SimpleProcessor.cpp
@@ -169,43 +169,48 @@ void run_simple_processor() {
 
   std::regex regex_pattern(".+,");
 
-  std::vector<double> *data_in = new std::vector<double>(100e3, 0.0);
-
+  std::vector<double> *data_in = nullptr;
+  
   try {
-
+    
+    data_in = new std::vector<double>(NUM_SAMPLES, 0.0);
+    
     file.open("time.csv", std::ifstream::in);
 
+    file.getline(buffer, BUFFER_SIZE);
+
+    while (file.good()) {
+      
+      if (counter >= 0) {
+
+        data_in->at(counter)= std::stod( std::regex_replace(buffer, regex_pattern , "") );
+        
+      }
+      
+      file.getline(buffer, BUFFER_SIZE);
+      
+      counter += 1;
+      
+    }
+
+    SimpleProcessor<double> proc( *data_in); 
+
+    // proc.print_stdout();
+
+    proc.dft();
+
+    file.close(); 
+    
   } catch(const std::system_error &e) {
     
     std::cerr << "Caught system error: " << e.what() << '\n';
-   
-  }
-
-  file.getline(buffer, BUFFER_SIZE);
-
-  while (file.good()) {
-    
-    if (counter >= 0) {
-
-      data_in->at(counter)= std::stod( std::regex_replace(buffer, regex_pattern , "") );
-      
-    }
-    
-    file.getline(buffer, BUFFER_SIZE);
-    
-    counter += 1;
     
   }
 
-  data_in->resize(NUM_SAMPLES);
+  if (data_in) {
 
-  SimpleProcessor<double> proc( *data_in); 
+    delete data_in;
 
-  // proc.print_stdout();
+  }
 
-  proc.dft();
-
-  file.close(); 
-
-  delete data_in;
 }
